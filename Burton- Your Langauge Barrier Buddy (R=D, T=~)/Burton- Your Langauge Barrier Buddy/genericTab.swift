3b4252b3530414081dd8564db01d5272087db610
LiamBlenx101 <lnblen@essex.ac.uk>
2018-04-25 05:18:49 +0100

    Burton - Prototype

---
+++ Burton- Your Langauge Barrier Buddy (R=D, T=~)/Burton- Your Langauge Barrier Buddy/genericTab.swift
@@ -0,0 +1,21 @@
+//
+//  GenericTab.swift
+//  Burton- Your Langauge Barrier Buddy
+//
+//  Created by Liam Blenx on 01/01/2017.
+//  Copyright © 2017 Blenx Industries. All rights reserved.
+//
+
+import UIKit
+
+class GenericTab: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
